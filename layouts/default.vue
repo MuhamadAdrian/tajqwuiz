a55dff9e6432f60c1800b220e25558f933fa7879
MuhamadAdrian <madrian.septiana@gmail.com>
2021-01-07 08:08:29 +0700

    adding refresh listener

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,6 +29,17 @@ export default {
 			html.classList.remove("dark");
 		}
 	},
+	async mounted() {
+		const workbox = await window.$workbox;
+		if (workbox) {
+			workbox.addEventListener("installed", (event) => {
+				// If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
+				if (event.isUpdate) {
+					store.commit("SET_IS_UPDATED", true);
+				}
+			});
+		}
+	},
 };
 </script>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   css: ["~/assets/main.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ["~/plugins/swListener.js"],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
